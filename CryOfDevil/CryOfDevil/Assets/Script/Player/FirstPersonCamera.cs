46da24ab1a85195a3071201ef0c64dda060cb410
Victória Mai <victoriamkvieira@gmail.com>
2022-05-03 20:30:06 -0300

    testeeee

---
+++ CryOfDevil/CryOfDevil/Assets/Script/Player/FirstPersonCamera.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public Transform characterBody;
+    public Transform characterHead;   
+
+    public float sensitivityY = 2.5f;
+    public float sensitivityX = 2.5f;
+
+    // aumenta ou diminui o angulo de rotação; que deve acumular a nossa variavel a cada frame.
+    float rotationX = 0;
+    float rotationY = 0;
+
+    float angleYmin = -65;
+    float angleYmax = 70;
+    
+    public bool firstPerson = true;
+
+    void Start()
+    {
+        Cursor.visible = false; // pra tirar o mouse da tela
+        Cursor.lockState = CursorLockMode.Locked; // que o mouse nao saia do centro quando movimentado dentro da aplicação em exe.               
+    }
+
+    // método que faz um objeto ser acompanhado com uma atualização de posição "tempo real"
+    private void LateUpdate()
+    {
+        transform.position = characterHead.position;        
+    }
+
+
+    void Update()
+    {
+        // Duas variaveis que armazenam o deslocamento do mouse na movimentação.
+        float verticalDelta = Input.GetAxisRaw("Mouse Y") * sensitivityY;
+        float horizontalDelta = Input.GetAxisRaw("Mouse X") * sensitivityX;
+
+        rotationX += horizontalDelta;
+        rotationY += verticalDelta;
+
+        //limitador de rotação de camera
+        rotationY = Mathf.Clamp(rotationY, angleYmin, angleYmax);
+
+        //fazer o player se movimantar na mesma diração que a camera aponta
+        characterBody.localEulerAngles = new Vector3(0, rotationX, 0);
+
+
+
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+    }
+}
---
+++ CryOfDevil/CryOfDevil/Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController _controller;
+    private PlayerAnimationController _playerAnim;
+
+    [SerializeField]
+    private float _Speed = 10f;
+    [SerializeField]
+    private float _maxSpeed = 6f;
+    [SerializeField]
+    private float _minSpeed = 2f;
+    private float _accelerationSpeed = 10f;
+    private float _decelerationSpeed = 10f;
+
+    Vector3 forward;
+    Vector3 strafe;
+    Vector3 vertical;
+
+    Vector3 _Running;
+
+    private float forwardSpeed_ = 4f;
+    private float strafeSpeed_ = 4f;
+
+
+    void Start() //onde comeca tudo do game - starto o game tudo comeca junto 
+    {
+        _controller = GetComponent<CharacterController>();
+        _playerAnim = GetComponent<PlayerAnimationController>();
+    }
+
+    void Update() //update do momento
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");        
+             
+        HandlePlaceMovements(horizontal, vertical);
+    }
+
+    void HandlePlaceMovements(float horizontal, float vertical)
+    {        
+        forward = vertical * forwardSpeed_ * transform.forward;
+        strafe = horizontal * strafeSpeed_ * transform.right;
+                 
+        Vector3 finalVelocity = forward + strafe;
+
+        WalkToRun();
+
+        _controller.Move(finalVelocity * Time.deltaTime);
+    }
+
+    void WalkToRun()
+    {
+        if (_playerAnim.IsRunning && _Speed < _maxSpeed)
+        {
+            _Speed += _accelerationSpeed * Time.deltaTime;
+        }
+
+        if (!_playerAnim.IsRunning && _Speed > _minSpeed)
+        {
+            if (_Speed < _minSpeed)
+            {
+                _Speed = _minSpeed;
+            }
+            else
+            {
+                _Speed -= _decelerationSpeed * Time.deltaTime;
+            }
+        }
+    }
+}
+
