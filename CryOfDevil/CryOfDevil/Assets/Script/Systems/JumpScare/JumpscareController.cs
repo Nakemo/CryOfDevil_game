781df68fc75f6833eea3adf1575833f28e6edd5f
Victória Mai <victoriamkvieira@gmail.com>
2022-05-09 14:09:58 -0300

    CryOfDevil_Game_backup_2

---
+++ CryOfDevil/CryOfDevil/Assets/Script/Systems/JumpScare/JumpscareController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpscareController : MonoBehaviour
+{
+    public GameObject _animation;
+
+    void OnTriggerEnter()
+    {
+
+    }
+}
