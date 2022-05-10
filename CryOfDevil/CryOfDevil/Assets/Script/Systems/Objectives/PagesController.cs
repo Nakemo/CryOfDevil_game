9d0ac9eb9f20143c0ee04feb3fc7244672938ed3
Nakati Mat <matheus.nakati.b2@gmail.com>
2022-05-10 00:19:24 -0300

    CryOfDevil_Game_V0.3.2_PageAdd

--- CryOfDevil/CryOfDevil/Assets/Script/Systems/Objectives/PagesController.cs
+++ CryOfDevil/CryOfDevil/Assets/Script/Systems/Objectives/PagesController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PagesController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public PlayerConfig playerConfig;
+    public GameObject[] pages;
+    
     void Start()
     {
-        
+        playerConfig = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConfig>();
+        pages = GameObject.FindGameObjectsWithTag("Pages");
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
